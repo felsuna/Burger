ac78792563f2e6bd5a7b2a489d9dfeea95eea324
felsuna <felicia.sunanto@gmail.com>
2019-02-07 17:54:50 -0700

    connecting jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,25 @@
 //Set up for MySQL connection.
 const mysql = require("mysql");
+const connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+// Connecting project to JawsDB
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
 
 // Make connection.
-connetion.connect((err)=>{
+connection.connect((err) => {
     if (err) throw err;
-    
-    console.log(`Connected as id: ${connection.threadId}`); 
+
+    console.log(`Connected as id: ${connection.threadId}`);
 });
 
 //Export connection to the ORM to use.
